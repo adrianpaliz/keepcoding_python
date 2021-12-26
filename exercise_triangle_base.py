90e069feec7b30e70a2ecfb580065977e2d1bcda
adrianpaliz <adrianpaliz@gmail.com>
2021-12-25 21:33:04 -0500

    version 2 using nested IF statements and not quit().

--- exercise_triangle_base.py
+++ exercise_triangle_base.py
@@ -8,19 +8,16 @@ def is_decimal(number):
 input_base = input("Triangle base: ")
 if is_decimal(input_base):
     base = float(input_base)
+    
+    input_height = input("Triangle height: ")
+    
+    if is_decimal(input_height):
+        height = float(input_height)
+        area = base * height / 2
+        
+        print("Triangle surface: ", round(area, 2))
+
+    else:
+        print(input_height, "must be a number")
 else:
     print(input_base, "must be a number")
-    quit()
-    
-input_height = input("Triangle height: ")
-if is_decimal(input_height):
-    height = float(input_height)
-else:
-    print(input_height, "must be a number")
-    quit()
-
-height = float(input_height)
-
-area = base * height / 2
-
-print("Triangle surface: ", round(area, 2))
