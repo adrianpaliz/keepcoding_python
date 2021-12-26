b5ef11c0896ebbbe6b4eabd85311ea9b26be12a3
adrianpaliz <adrianpaliz@gmail.com>
2021-12-25 21:10:48 -0500

    Exercise about diferent ways for calcule triangle's base.

---
+++ exercise_triangle_base.py
@@ -0,0 +1,9 @@
+input_base = input("Triangle base: ")
+input_height = input("Triangle height: ")
+
+base = float(input_base)
+height = float(input_height)
+
+area = base * height / 2
+
+print("Triangle surface: ", round(area, 2))
