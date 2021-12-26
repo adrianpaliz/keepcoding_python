3336c488940efccf367209081b6eb9681f1c7abc
adrianpaliz <adrianpaliz@gmail.com>
2021-12-25 21:25:31 -0500

    Version using functions, if, else, quit().

--- exercise_triangle_base.py
+++ exercise_triangle_base.py
@@ -1,7 +1,24 @@
+def is_decimal(number):
+    try:
+        float(number)
+        return True
+    except:
+        return False
+    
 input_base = input("Triangle base: ")
+if is_decimal(input_base):
+    base = float(input_base)
+else:
+    print(input_base, "must be a number")
+    quit()
+    
 input_height = input("Triangle height: ")
+if is_decimal(input_height):
+    height = float(input_height)
+else:
+    print(input_height, "must be a number")
+    quit()
 
-base = float(input_base)
 height = float(input_height)
 
 area = base * height / 2
