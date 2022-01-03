856c8bc49781c3ad7637925983528b530ef82344
adrianpaliz <adrianpaliz@gmail.com>
2022-01-03 08:32:47 -0500

    Version 1 using accumulators for print.

--- exercise_bill_0.py
+++ exercise_bill_0.py
@@ -7,10 +7,14 @@ price_float = float(price_input)
 total_items = 0
 total_price = 0
 
+print_lines = "\n"
+
 while units_float > 0 and price_float > 0:
     unitary_total = units_float * price_float
-    print(price_float, "€ -", units_float, "units -", unitary_total, "€")
-        
+    
+    print_lines += str(price_float) + "€ - " + str(units_float) + " units - " + str(unitary_total) + "€\n"
+    # Another way for print_lines with .formart
+    # print_lines += "{}€ - {} units - {}€\n".format(precio_float, units_float, unitary_total)
     total_items += units_float
     total_price += unitary_total
     
@@ -19,6 +23,7 @@ while units_float > 0 and price_float > 0:
     price_input = input("Unit price (€): ")
     price_float = float(price_input)
 
+print(print_lines)
 print("-------------------")
 print("Total:", total_price)
 print("Units:", total_items)
