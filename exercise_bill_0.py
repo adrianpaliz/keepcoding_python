481f8b51fa74738e9448188f9ff1bf6aefa017c5
adrianpaliz <adrianpaliz@gmail.com>
2022-01-03 11:12:34 -0500

    Version 4 using list of lists and constans.

--- exercise_bill_0.py
+++ exercise_bill_0.py
@@ -1,3 +1,6 @@
+UNITS = 1
+PRICE = 0
+
 units_input = input("Amount: ")
 units_float = float(units_input)
 
@@ -7,13 +10,16 @@ price_float = float(price_input)
 total_items = 0
 total_price = 0
 
-prices_list = []
-units_list = []
+bill_lines_list = []
+
 while units_float > 0 and price_float > 0:
     unitary_total = units_float * price_float
-    prices_list.append(price_float)
-    units_list.append(units_float)
+    item = []
+    item.append(units_float)
+    item.append(price_float)
     
+    bill_lines_list.append(item)
+        
     total_items += units_float
     total_price += unitary_total
     
@@ -22,8 +28,8 @@ while units_float > 0 and price_float > 0:
     price_input = input("Unit price (€): ")
     price_float = float(price_input)
 
-for price_float, units_float in zip(prices_list, units_list):
-    print(price_float, "€ -", units_float, "units -", price_float * units_float, "€")
+for item in bill_lines_list:
+    print(item[UNITS],"€ -", item[PRICE], "unidades -", item[PRICE] * item[UNITS], "€")
 
 print("-------------------")
 print("Total:", total_price)
