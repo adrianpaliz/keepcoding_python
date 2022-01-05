b7424142fb84f648c454571d72f16755275e171b
adrianpaliz <adrianpaliz@gmail.com>
2022-01-04 19:15:15 -0500

    Version for convert from lowercase to uppercase.

--- uppercase_to_lowercase_exercice_0.0.1.py
+++ uppercase_to_lowercase_exercice_0.0.1.py
@@ -1,16 +1,16 @@
-def to_uppercase(word):
+def to_lowercase(word):
     result = ""
     
     for character in word:
         character_code = ord(character)
-        if character_code >= 97 and character_code <= 122:        
-            uppercase_code = character_code - 32
-            uppercase_character = chr(uppercase_code)
-            result += uppercase_character
+        if character_code >= 65 and character_code <= 90:
+            lowercase_code = character_code + 32
+            lowercase_character = chr(lowercase_code)
+            result += lowercase_character
         else:
             result += character
-    
+
     return result
 
-words_input = input("Word for trun into uppercase: ")
-print("Your uppercase word is:", to_uppercase(words_input))
+words_input = input("Word for trun into lowercase: ")
+print("Your lowercase word is:", to_lowercase(words_input))
