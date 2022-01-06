d9018c9adfd19a93acfb7c8c3568eab0f28bef78
adrianpaliz <adrianpaliz@gmail.com>
2022-01-06 18:41:07 -0500

    convert text to hexadecimal values and print the positions 2 and 3.

--- mars0.py
+++ mars0.py
@@ -2,4 +2,8 @@ message = "alive"
 
 for character in message:
     hexadecimal_value = hex(ord(character))
-    print(hexadecimal_value)
+    digit_1 = hexadecimal_value[2]
+    digit_2 = hexadecimal_value[3]
+
+    print(digit_1)
+    print(digit_2)
