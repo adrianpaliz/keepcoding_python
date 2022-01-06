b0330c0fe2b4c713d2cc07e3455dadc6c77b427b
adrianpaliz <adrianpaliz@gmail.com>
2022-01-06 18:48:11 -0500

    Now we print the 2 and 3 positions of hexadeciaml value with an angle.

--- mars0.py
+++ mars0.py
@@ -1,9 +1,14 @@
+digits_16 = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
+angle = 360 / 16
 message = "alive"
 
 for character in message:
     hexadecimal_value = hex(ord(character))
     digit_1 = hexadecimal_value[2]
+    angle_1 = digits_16.index(digit_1) * angle
+    
     digit_2 = hexadecimal_value[3]
-
-    print(digit_1)
-    print(digit_2)
+    angle_2 = digits_16.index(digit_2) * angle
+    
+    print(digit_1, '-', angle_1)
+    print(digit_2, '-', angle_2)
