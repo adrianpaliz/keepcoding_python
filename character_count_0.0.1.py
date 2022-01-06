2990c305bea6534c603cdcca0a9201cd53bc68d1
adrianpaliz <adrianpaliz@gmail.com>
2022-01-06 10:30:25 -0500

    Count characters using dictionaries.

--- character_count_0.0.1.py
+++ character_count_0.0.1.py
@@ -1,26 +1,12 @@
-def exists(letter, table):
-    position = 0
-    while position < len(table):
-        if table[position] == letter:
-            return position
-        position += 1
-    
-    return None
-
 sentence = "Real change, enduring change, happens one step at a time." # Quote by Ruth Bader Ginsburg
 
-letters = []
-frequencies = []
+frequencies = dict()
 
 for character in sentence:
-    position = exists(character, letters)
-    if position != None:
-        frequencies[position] += 1
+    if character in frequencies:
+        frequencies[character] += 1
     else:
-        letters.append(character)
-        frequencies.append(1)
-
-index = 0
-while index < len(letters):
-    print(letters[index], "-", frequencies[index])
-    index += 1
+        frequencies[character] = 1
+        
+for letter in frequencies.keys():
+    print(letter, "-", frequencies[letter])
