da8e1b7734dea0cfe241d5ff94ee0b41112073a8
adrianpaliz <adrianpaliz@gmail.com>
2022-01-06 19:17:12 -0500

    Convert hexadecimal values to text.

--- mars0.py
+++ mars0.py
@@ -1,14 +1,5 @@
-digits_16 = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f')
-angle = 360 / 16
-message = "alive"
-
-for character in message:
-    hexadecimal_value = hex(ord(character))
-    digit_1 = hexadecimal_value[2]
-    angle_1 = digits_16.index(digit_1) * angle
-    
-    digit_2 = hexadecimal_value[3]
-    angle_2 = digits_16.index(digit_2) * angle
-    
-    print(digit_1, '-', angle_1)
-    print(digit_2, '-', angle_2)
+message = "61 6c 69 76 65"
+hexadecimal_string = message
+bytes_object = bytes.fromhex(hexadecimal_string)
+ascii_string = bytes_object.decode()
+print(ascii_string)
