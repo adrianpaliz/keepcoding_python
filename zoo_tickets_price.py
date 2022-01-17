96b1fa24d773803acfbf18810098f0b7229818fd
adrianpaliz <adrianpaliz@gmail.com>
2022-01-17 15:24:24 -0500

    We start to use the zoo_tickets_module for print, clean and format.

--- zoo_tickets_price.py
+++ zoo_tickets_price.py
@@ -1,3 +1,5 @@
+import zoo_tickets_module
+
 def ticket_price_calculation(age):
     if age > 0 and age <= 2:
         price = 0
@@ -19,21 +21,30 @@ def age_imput_validation(raw_input):
         return False
 
 def age_imput():
+    zoo_tickets_module.locate_on_display(1, 1)
     age = input("How old you are?: ")
     while age_imput_validation(age) == False:
         print("Wrong age")
+        zoo_tickets_module.locate_on_display(1, 1)
         age = input("How old you are?: ")
     return int(age)
+
+zoo_tickets_module.clear_display()
 
 age = age_imput()
 
 total_price = 0
 
+line = 4
+
 while age != 0:
     ticket_price = ticket_price_calculation(age)
+    zoo_tickets_module.locate_on_display(line, 1)
     print("Ticket price: {}".format(ticket_price))
+    line += 1
     total_price += ticket_price
 
     age = age_imput()
 
+zoo_tickets_module.locate_on_display(line, 8)
 print("Total: {}".format(total_price))
