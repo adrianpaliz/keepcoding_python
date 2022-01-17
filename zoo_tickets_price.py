5ea8f981a9cf6102aa0c0c76008b4b56468c58a5
adrianpaliz <adrianpaliz@gmail.com>
2022-01-17 15:33:09 -0500

    Changes on te while loop for print each item using the zoo_tickets_module.

--- zoo_tickets_price.py
+++ zoo_tickets_price.py
@@ -37,14 +37,37 @@ total_price = 0
 
 line = 4
 
+line_number_baby = 0
+line_number_kid = 0
+line_number_adult = 0
+line_number_senior = 0
+
 while age != 0:
     ticket_price = ticket_price_calculation(age)
-    zoo_tickets_module.locate_on_display(line, 1)
-    print("Ticket price: {}".format(ticket_price))
+    if ticket_price == 0:
+        line = 4
+        line_number_baby += 1
+        zoo_tickets_module.locate_on_display(line, 1)
+        print("Ticket price: {} \tTickets number: {}".format(ticket_price, line_number_baby))
+    if ticket_price == 14:
+        line = 5
+        line_number_kid += 1
+        zoo_tickets_module.locate_on_display(line, 1)
+        print("Ticket price: {}\tTickets number: {}".format(ticket_price, line_number_kid))
+    if ticket_price == 23:
+        line = 6
+        line_number_adult += 1
+        zoo_tickets_module.locate_on_display(line, 1)
+        print("Ticket price: {}\tTickets number: {}".format(ticket_price, line_number_adult))
+    if ticket_price == 18:
+        line = 7
+        line_number_senior += 1
+        zoo_tickets_module.locate_on_display(line, 1)
+        print("Ticket price: {}\tTickets number: {}".format(ticket_price, line_number_senior))
     line += 1
     total_price += ticket_price
 
     age = age_imput()
 
-zoo_tickets_module.locate_on_display(line, 8)
+zoo_tickets_module.locate_on_display(line, 1)
 print("Total: {}".format(total_price))
