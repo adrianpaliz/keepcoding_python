f0e372d9622fd5a87fd0f0ee59ece519e7f19bd4
adrianpaliz <adrianpaliz@gmail.com>
2022-01-18 22:32:07 -0500

    New module whith 2 dictionaries values that they were part of zoo_tickets_price.

---
+++ zoo_tickets_configuration.py
@@ -0,0 +1,25 @@
+tickets_prices = {
+    'baby' : 0.0,
+    'kid' : 14.0,
+    'adult' : 23.00,
+    'senior' : 18.00
+    }
+
+amount_of_tickets = {
+    'baby' : {
+        'tickets_number' : (4, 15),
+        'tickets_price' : (4, 19)
+        },
+    'kid' : {
+        'tickets_number' : (5, 15),
+        'tickets_price' : (5, 19)
+        },
+    'adult' : {
+        'tickets_number' : (6, 15),
+        'tickets_price' : (6, 19)
+        },
+    'senior' : {
+        'tickets_number' : (7, 15),
+        'tickets_price' : (7, 19)
+        }
+    }
