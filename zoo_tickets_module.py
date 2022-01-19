746c90188e77dfc9dd9d0efc85d4dae6c6680422
adrianpaliz <adrianpaliz@gmail.com>
2022-01-18 22:00:34 -0500

    Import click package, create 3 dictionaries, a new function was defined and make changes on other three.

--- zoo_tickets_module.py
+++ zoo_tickets_module.py
@@ -1,3 +1,36 @@
+import click
+colors = {
+    'black' : '30',
+    'red' : '31',
+    'green' : '32',
+    'yellow' : '33',
+    'blue' : '34',
+    'magenta' : '35',
+    'cyan' : '36',
+    'while' : '37',
+    'reset' : '39'    
+    }
+
+background = {
+    'black' : '40',
+    'red' : '41',
+    'green' : '42',
+    'yellow' : '43',
+    'blue' : '44',
+    'magenta' : '45',
+    'cyan' : '46',
+    'while' : '47',
+    'reset' : '49'   
+    }
+
+styles = {
+    'bold' : 1,
+    'underline' : 4,
+    'blink' : 5,
+    'reversed' : 7    
+    }
+
+
 def clear_display():
     print("\033[2J")
 
@@ -10,22 +43,44 @@ def clear_line(line = None, column = None):
     elif line is not None:
         locate_on_display(line, 1)
         
-    print("\033[K", end = "")
-
-def print_location(string, line, column, delete_end = False):
-    locate_on_display(line, column)
-    if delete_end:
-        clear_line()
-    print(string, end = "")
+    print("\033[K", end = "")    
     
-def input_location(string, line, column, delete_end = True):
-    locate_on_display(line, column)
-    if delete_end:
-        clear_line()
-    return input(string)
+def process_parameters(parameters):
+    if 'line' in parameters:
+        line = parameters['line']
+        column = 1
+        if 'column' in parameters:
+            column = parameters['column']
+        locate_on_display(line, column)
+    
+    if 'color' in parameters and parameters['color'] in colors:
+        print("\033[{}m".format(colors[parameters['color']]), end ="")
+    
+    if 'background' in parameters and parameters['background'] in background:
+        print("\033[{}m".format(background[parameters['background']]), end ="")
+    
+    if 'style' in parameters and parameters['style'] in styles:
+        print("\033[{}m".format(styles[parameters['style']]), end ="")        
+    
 
-def text_format(text_style, text_color = 37, background_color = 40):
-    print("\033[{};{};{}m".format(text_style, text_color, background_color), end = "")
+def print_location(string, **parameters):
+    process_parameters(parameters)
+    if 'new_line' in parameters and parameters['new_line']:
+        print(string)
+    else:
+        print(string, end="")
+    
+    if 'new_return' not in parameters:
+        reset_text_format()    
+    
+def input_location(message, **parameters):
+    process_parameters(parameters)
+    if 'data' not in parameters:
+        clear_line()
+    return input(message)
+    
+def text_format(style, text_color = 'white', background_color = 'black'):
+    click.style
     
 def reset_text_format():
-    text_format(0, 37, 40)
+    print("\033[0m", end = "")
