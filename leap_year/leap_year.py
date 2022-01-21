b57ff04e30355f470848ed8125f2867824e1356b
adrianpaliz <adrianpaliz@gmail.com>
2022-01-21 17:19:51 -0500

    Final version with formatted print string.

--- leap_year/leap_year.py
+++ leap_year/leap_year.py
@@ -1,17 +1,16 @@
 print("Enter the year for ckeck if is leap")
-
 year = int(input("> "))
 
 def is_leap(year):
     if (year / 4) ==  int(year / 4):
         if (year / 100) == int(year / 100):
             if (year / 400) == int(year / 400):
-                return True              
+                print("The year {} is leap.".format(year))
             else:
-                return False             
+                print("The year {} is not leap.".format(year))
         else:
-            return True           
+            print("The year {} is leap.".format(year))
     else:
-        return False
-                  
-print(is_leap(year))
+        print("The year {} is not leap.".format(year))
+
+is_leap(year)
