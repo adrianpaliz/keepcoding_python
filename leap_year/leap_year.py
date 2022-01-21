04e20032b59e411d23a748b2e38f1e256677a118
adrianpaliz <adrianpaliz@gmail.com>
2022-01-21 17:11:22 -0500

    Improved function, without erros.

--- leap_year/leap_year.py
+++ leap_year/leap_year.py
@@ -3,10 +3,12 @@ print("Enter the year for ckeck if is leap")
 year = int(input("> "))
 
 def is_leap(year):
-    if year / 4 == int(year / 4):
-        if year / 400 == int(year / 400):
-            return True
-            if year / 100 == int(year / 100):
-                return False
+    if (year / 4) ==  int(year / 4):
+        if (year / 100) == int(year / 100):
+            if (year / 400) == int(year / 400):
+                return True
+            return False
+        return True
+    return False
 
 print(is_leap(year))
