6446486c0de1c21336875751eec6c62a12e97d33
adrianpaliz <adrianpaliz@gmail.com>
2022-01-21 20:29:45 -0500

    Add another basic function and also boilerplate code for use as a module.

--- advanced_functions/advanced_functions.py
+++ advanced_functions/advanced_functions.py
@@ -4,11 +4,16 @@ def return_integer(integer):
 def square(integer):
     return integer * integer
 
+def cube(integer):
+    return integer**3
+
 def sum_all_integers_advanced(limit_to, function):
     result = 0
     for integer in range(limit_to + 1):
         result += function(integer)
     return result
 
-print(sum_all_integers_advanced(100, return_integer))
-print(sum_all_integers_advanced(3, square))
+if __name__ == '__main__':
+    print(sum_all_integers_advanced(100, return_integer))
+    print(sum_all_integers_advanced(3, square))
+    print(sum_all_integers_advanced(3, cube))
