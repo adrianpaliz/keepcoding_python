15d64dbb30b46c2321c5f7c12ca23c06a70fa886
adrianpaliz <adrianpaliz@gmail.com>
2022-01-21 21:05:23 -0500

    We import one function from the module and use lambda.

---
+++ advanced_functions/advanced_functions_2.py
@@ -0,0 +1,4 @@
+from advanced_functions import sum_all_integers_advanced
+
+print(sum_all_integers_advanced(3, lambda argument : argument * 2))
+print(sum_all_integers_advanced(100, lambda argument : argument * 3))
