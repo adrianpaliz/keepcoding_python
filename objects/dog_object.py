c3b030f28c8d295b2605a9ae02f77c622626d230
adrianpaliz <adrianpaliz@gmail.com>
2022-01-27 18:29:23 -0500

    Add a class that inherits the functionality from another class.

--- objects/dog_object.py
+++ objects/dog_object.py
@@ -6,9 +6,27 @@ class Dog():
 
     def bark(self):
         if self.weight >= 8:
-            print("GUAU GUAU")
+            print("GUAU, GUAU.")
         else:
-            print("guau, guau")
+            print("guau, guau.")
 
     def __str__(self):
         return "Dog name {}, age: {}, weight: {}.".format(self.name, self.age, self.weight)
+
+class Assistance_dog(Dog):
+    def __init__(self, name, age, weight, owner):
+        Dog.__init__(self, name, age, weight)
+        self.owner = owner
+        self.working = False
+
+    def __str__(self):
+        return "Dog name {}, is a assistance dog of {}.".format(self.name, self.owner)
+
+    def walk(self):
+        print("OK, I {} help my owner, {} to walk".format(self.name, self.owner))
+
+    def bark(self):
+        if self.working:
+            print("Shhhh, I can't bark")
+        else:
+            Dog.bark(self)
