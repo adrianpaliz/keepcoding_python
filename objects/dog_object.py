17172f8ebe6599616f24ca5dc71d8ae5d0f9fc27
adrianpaliz <adrianpaliz@gmail.com>
2022-01-27 18:59:02 -0500

    Add another class with properties with None as value.

--- objects/dog_object.py
+++ objects/dog_object.py
@@ -1,3 +1,18 @@
+class Another_dog():
+    def __init__(self):
+        self.name = ""
+        self.age = None
+        self.weight = None
+
+    def bark(self):
+        if self.weight == None:
+            print("I'm a gosht.")
+
+        if self.weight >= 8:
+            print("GUAU, GUAU.")
+        else:
+            print("guau, guau.")
+
 class Dog():
     def __init__(self, name, age, weight):
         self.name = name
@@ -17,16 +32,22 @@ class Assistance_dog(Dog):
     def __init__(self, name, age, weight, owner):
         Dog.__init__(self, name, age, weight)
         self.owner = owner
-        self.working = False
+        self.__working = False
 
     def __str__(self):
-        return "Dog name {}, is a assistance dog of {}.".format(self.name, self.owner)
+        return "Assistance dog of {}.".format(self.owner)
 
     def walk(self):
         print("OK, I {} help my owner, {} to walk".format(self.name, self.owner))
 
     def bark(self):
-        if self.working:
+        if self.__working:
             print("Shhhh, I can't bark")
         else:
             Dog.bark(self)
+
+    def working(self, value = None):
+        if value == None:
+            return self.__working
+        else:
+            self.__working = value
