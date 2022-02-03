430da199b157d2ad935d99a234d499427c53d835
adrianpaliz <adrianpaliz@gmail.com>
2022-02-02 22:24:36 -0500

    Added a tuple, changes on the methods competitor, game and compete of the classes

--- classes/animal_race.py
+++ classes/animal_race.py
@@ -2,10 +2,16 @@ import pygame, sys, random
 
 
 class Competitor:
+    __costumes = ("turtle", "fish", "prawn", "moray", "octopus")
+
     def __init__(self, x_axis=0, y_axis=0):
-        self.costume = pygame.image.load("images/turtle.png")
+        costume_index = random.randint(0, 4)
+
+        self.costume = pygame.image.load(
+            "images/{}.png".format(self.__costumes[costume_index])
+        )
         self.position = [x_axis, y_axis]
-        self.name = "Turtle"
+        self.name = ""
 
     def move_forward(self):
         self.position[0] += random.randint(1, 6)
@@ -13,6 +19,8 @@ class Competitor:
 
 class Game:
     competitors = []
+    __start_position_y_axis = (160, 200, 240, 280)
+    __names = ("Percanta", "Tarzan", "Inty", "Chimborazo")
     __start_line = 5
     __finish_line = 620
 
@@ -21,9 +29,16 @@ class Game:
         self.__background = pygame.image.load("images/background.png")
         pygame.display.set_caption("Animal race.")
 
-        first_competitor = Competitor(self.__start_line, 240)
-        first_competitor.name = "Chimborazo"
-        self.competitors.append(first_competitor)
+        for chosen_one in range(4):
+            competitor = Competitor(
+                self.__start_line, self.__start_position_y_axis[chosen_one]
+            )
+            competitor.name = self.__names[chosen_one]
+            self.competitors.append(competitor)
+
+    def close(self):
+        pygame.quit()
+        sys.exit()
 
     def compete(self):
         there_is_a_winner = False
@@ -32,21 +47,23 @@ class Game:
                 if event.type == pygame.QUIT:
                     there_is_a_winner = True
 
-            self.competitors[0].move_forward()
-
-            if self.competitors[0].position[0] >= self.__finish_line:
-                print("{} has won the race.".format(self.competitors[0].name))
-                there_is_a_winner = True
+            for competitor in self.competitors:
+                competitor.move_forward()
+                if competitor.position[0] >= self.__finish_line:
+                    print("{} has won the race.".format(competitor.name))
+                    there_is_a_winner = True
 
             self.__screen.blit(self.__background, (0, 0))
-            self.__screen.blit(
-                self.competitors[0].costume, self.competitors[0].position
-            )
+
+            for competitor in self.competitors:
+                self.__screen.blit(competitor.costume, competitor.position)
 
             pygame.display.flip()
 
-        pygame.quit()
-        sys.exit()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.close()
 
 
 if __name__ == "__main__":
